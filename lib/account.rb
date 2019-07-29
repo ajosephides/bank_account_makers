93cd81675e3fb3af0940d9059d96f3a625b12bb6
Alexis Josephides <ajosephides@gmail.com>
2019-07-29 13:36:26 +0100

    feature: added a simple deposit feature

--- lib/account.rb
+++ lib/account.rb
@@ -1,5 +1,16 @@
 class Account
+  attr_reader :balance
 
+  def initialize
+    @balance = 0
+  end
+
+
+
+  def deposit(amount, date)
+    @balance = @balance + amount  
+  end
+  
   def print
     return 'date || credit || debit || balance'
   end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -7,4 +7,17 @@ describe Account do
       expect(account.print).to eq('date || credit || debit || balance')
     end
   end
+
+  describe '#deposit' do
+    it 'allow a customer to add credit to their account and their balance will increase' do
+      account = Account.new
+      account.deposit(1000.00, '10/01/2012')
+      expect(account.balance).to eq(1000.00)
+    end
+  end
+
+
+
+
 end
+
