f77b335e6ac7a5b4811d675212a2c722d3502cf5
Alexis Josephides <ajosephides@gmail.com>
2019-07-29 13:59:45 +0100

    test refactor: dry refactor

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,16 +1,16 @@
 require 'account'
 
 describe Account do
+
+  let(:account) {Account.new}
   describe '#print' do
     it 'should be able to print a statement with correct headings' do
-      account = Account.new
       expect(account.print).to eq('date || credit || debit || balance')
     end
   end
 
   describe '#deposit' do
     it 'allow a customer to add credit to their account and their balance will increase' do
-      account = Account.new
       account.deposit(1000.00, '10/01/2012')
       expect(account.balance).to eq(1000.00)
     end
@@ -18,7 +18,6 @@ describe Account do
 
   describe '#withdraw' do
     it 'allow a customer to add credit to their account and their balance will increase' do
-      account = Account.new
       account.deposit(1000.00, '10/01/2012')
       account.withdraw(500.00, '11/01/2012')
       expect(account.balance).to eq(500.00)
